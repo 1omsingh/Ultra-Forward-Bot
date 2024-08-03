5539a10e3161d9cdbfdb176b90f20740e0d1842d
1omsingh <114354808+1omsingh@users.noreply.github.com>
2024-08-03 22:23:16 +0530

    Update config.py

--- config.py
+++ config.py
@@ -9,13 +9,13 @@
 import os
 
 class Config:
-    API_ID = os.environ.get("API_ID", "")
-    API_HASH = os.environ.get("API_HASH", "")
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "") 
+    API_ID = os.environ.get("API_ID", "26426018")
+    API_HASH = os.environ.get("API_HASH", "db3addaeb6a7d9a1c8c4d5be80f68d8f")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "7255526540:AAHr9ywX-k8QlG_fEv8n0wRLczwcKqnGYqI") 
     BOT_SESSION = os.environ.get("BOT_SESSION", "forward-bot") 
-    DB_URL = os.environ.get("DB_URL", "")
-    DB_NAME = os.environ.get("DB_NAME", "madflixbotz")
-    OWNER_ID = [int(id) for id in os.environ.get("OWNER_ID", '').split()]
+    DB_URL = os.environ.get("DB_URL", "mongodb+srv://Om2323705:<password>@cluster0.bduwozx.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0mongodb+srv://Om2323705:oHu6KwfZ2Re7jGmf@cluster0.bduwozx.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+    DB_NAME = os.environ.get("DB_NAME", "@diligentsoul678")
+    OWNER_ID = [int(id) for id in os.environ.get("OWNER_ID", '6074862505').split()]
 
 
 class temp(object): 
@@ -33,6 +33,6 @@ class temp(object):
 
 # Jishu Developer 
 # Don't Remove Credit 🥺
-# Telegram Channel @Madflix_Bots
+# Telegram Channel @hdhhdyu_om
 # Backup Channel @JishuBotz
-# Developer @JishuDeveloper
+# Developer @diligentsoul678
